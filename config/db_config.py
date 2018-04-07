34f90c9302c5753f2492aa20f5d3706189b4f63c
cjdxsxd <3360048@163.com>
2018-04-07 16:06:59 +0800

    新增文件和修改文件

---
+++ config/db_config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+# @time 2018/4/7 16:04
+# @autor:shangxudong
+# @name: db_config.py
+# @software: PyCharm
+sql_connect={
+    "host":"127.0.0.1",
+    "port":"3306",
+    "username":"cjdxsxd",
+    "password":"Cjdxsxd421127",
+    "db":"testProj"
+}
